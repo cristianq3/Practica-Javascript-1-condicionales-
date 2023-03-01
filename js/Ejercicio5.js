48adc69498ec88b069cce86f5c2dae605903c3fd
cristianq3 <cristianq3@gmail.com>
2023-03-01 14:33:50 -0300

    Se agregan comentarios con las consignas y comentarios referidos a cada ejercicio

--- js/Ejercicio5.js
+++ js/Ejercicio5.js
@@ -1,8 +1,8 @@
+//Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+//Se declaran las variables y mediante un prompt le asignamos el nro que ingrese el usuario.
 let primerNum = parseInt(prompt("Ingresa el Primer Número"));
 let segundoNum = parseInt(prompt("Ingresa el Segundo Número"));
 
-document.write("La suma es " + (primerNum + segundoNum) );
-
-
-
-
+// Se muestra la suma de las variables.
+document.write("La suma es " + (primerNum + segundoNum));
